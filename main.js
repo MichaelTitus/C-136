514cc3a3fe9b790fd212ad0d2fe6ba1650f60865
MichaelTitus <76416268+MichaelTitus@users.noreply.github.com>
2021-08-10 11:35:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+video = "";
+function preload() {
+    video = createVideo('video.mp4')
+    video.hide()
+}
+function setup() {
+    canvas = createCanvas(480, 380)
+    canvas.center()
+
+}
+function draw() {
+    image(video, 0, 0, 480, 380)
+    console.log("video is live")
+}
+
+function start(){
+    objectdetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("model is loaded")
+    status = true;
+    video.loop()
+    video.speed(1)
+    video.volume(0)
+}
